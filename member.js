d26bdcfdc9fd43abcfb41a436e4336e743221b1e
Playingood <66518093+Playingood@users.noreply.github.com>
2024-07-16 15:50:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = {
+    'javascript': 'good',
+    'html': 'good',
+    'css': 'good',
+    'nodejs': 'good',
+    'mongodb': 'good'
+  };
+  return skills;
+}
